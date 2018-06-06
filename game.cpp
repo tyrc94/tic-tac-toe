cc251b4650b6bad7ac64da4d3b69d63e26e42126
Tyrell Carew <tcarew21@gmail.com>
2018-06-06 23:09:53 +0100

    game

---
+++ game.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Game{
+    vector <vector<int>> board;
+    int current_player;
+};
+
+void make_move(Game g, int x, int y){
+    int board_size = g.board.size();
+    if(x < 0 || x >= board_size) {
+        throw new exception();
+    }
+    if(y < 0 || y >= board_size){
+        throw new exception();
+    }
+    g.board[x][y] = g.current_player;
+    g.current_player = (g.current_player == 1) ? 1 : 2;
+}
+
+int game_over(Game g){
+    if(g.board[0][0] != 0 && g.board[0][0] == g.board[0][1] && g.board[0][0] == g.board[0][2]){
+        return g.board[0][0];
+    }
+    if(g.board[0][0] != 0 && g.board[0][0] == g.board[1][1] && g.board[0][0] == g.board[2][2]){
+        return g.board[0][0];
+    }
+    if(g.board[0][0] != 0 && g.board[0][0] == g.board[1][0] && g.board[0][0] == g.board[2][0]){
+        return g.board[0][0];
+    }
+    if(g.board[1][0] != 0 && g.board[1][0] == g.board[1][1] && g.board[1][0] == g.board[0][2]){
+        return g.board[0][0];
+    }
+    if(g.board[2][0] != 0 && g.board[2][0] == g.board[2][1] && g.board[2][0] == g.board[2][2]){
+        return g.board[0][0];
+    }    
+    if(g.board[0][1] != 0 && g.board[0][1] == g.board[1][1] && g.board[0][1] == g.board[2][1]){
+        return g.board[0][0];
+    }
+    if(g.board[0][2] != 0 && g.board[0][2] == g.board[1][2] && g.board[0][2] == g.board[2][2]){
+        return g.board[0][0];
+    }    
+    if(g.board[2][0] != 0 && g.board[2][0] == g.board[1][1] && g.board[2][0] == g.board[0][2]){
+        return g.board[0][0];
+    }
+    for(int x = 0; x < g.board.size(); x++){
+        for(int y = 0; y < g.board.size(); y++){
+            if(g.board[x][y] == 0){
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+void play_game(){
+    int game_state = -1;
+    while(game_state == -1){
+        int x, y;
+        cin >> x;
+        cin >> y;
+        cout << "You have played position (" << x << ", ";
+        cout << y << ")" << endl;
+        game_state = 1;
+    }
+}
+
+void show_board(Game g){
+
+}
+
+int main(){
+    play_game();
+}
---
+++ node.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node{
+    vector <vector <int>> state;
+    Node* parent = nullptr;
+    int player;
+    int score;
+    vector <Node> children;
+};
+
+void expand(Node* current_node) {
+    for(int i = 0; i < current_node -> state.size(); i++){
+        for(int j = 0; j < current_node -> state[i].size(); j++){
+            if(current_node -> state[i][j] == 0){
+                Node new_node;
+                new_node.player = (current_node -> player == 1)? 2 : 1;
+                vector <vector <int>> new_state;
+                new_state = current_node -> state;
+                new_state[i][j] = new_node.player;
+                new_node.state = new_state;
+                new_node.parent = current_node;
+                current_node -> children.push_back(new_node);
+
+                // for(int x = 0; x < new_state.size(); x++){
+                //     for(int y = 0; y < new_state[x].size(); y++){
+                //         cout << new_state[x][y];
+                //     }
+                //     cout << endl;
+                // }
+                // cout << endl;
+            }
+        }
+
+    }
+
+}
+
+
+int main(){
+    Node node;
+    node.state = {{1,0,0},{0,0,0},{1,1,1}};
+    node.children = {};
+    node.player = 1;
+    expand(&node);
+    cout << node.children.size() << endl;
+    for(Node& x : node.children){
+        expand(&x);
+    }
+    cout << "end" << endl;
+}
